43b22539f6618e2f7dc59e8f6beadc59d0dd9f2c
Adam Hellberg <sharparam@sharparam.com>
2025-01-25 01:18:14 +0100

    Add packages for different variants

--- flake.nix
+++ flake.nix
@@ -10,12 +10,58 @@
       utils,
       nixpkgs,
     }@inputs:
+    let
+      lib = nixpkgs.lib;
+      version = "1.0.0";
+    in
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        makeIosevkaFont = set: mode: plan: (
+          pkgs.buildNpmPackage {
+            pname = "iosevka-${set}-${mode}";
+            inherit version;
+
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              ttfautohint-nox
+            ];
+
+            npmDepsHash = "sha256-Qr7fN49qyaqaSutrdT7HjWis7jjwYR/S2kxkHs7EhXY=";
+
+            npmBuildFlags = ["--" "${mode}::${plan}"];
+
+            npmPackFlags = [ "--ignore-scripts" ];
+
+            installPhase = ''
+              runHook preInstall
+              fontdir="$out/share/fonts/TTC/Iosevka Sharpie"
+              install -d "$fontdir"
+              install "dist/.supeer-ttc"/* "$fontdir"
+              runHook postInstall
+            '';
+
+            meta = {
+              homepage = "https://github.com/Sharparam/Iosevka";
+              description = "Iosevka Sharpie";
+              license = lib.licenses.ofl;
+              platforms = lib.platforms.all;
+            };
+          }
+        );
       in
       {
+        packages = {
+          default = makeIosevkaFont "sharpie" "super-ttc" "IosevkaSharpie";
+          ttc = makeIosevkaFont "sharpie" "ttc" "IosevkaSharpie";
+          ttf = makeIosevkaFont "sharpie" "ttf" "IosevkaSharpie";
+          term-ttc = makeIosevkaFont "sharpie-term" "ttc" "IosevkaSharpieTerm";
+          fixed-ttc = makeIosevkaFont "sharpie-fixed" "ttc" "IosevkaSharpieFixed";
+          term-ttf = makeIosevkaFont "sharpie-term" "ttf" "IosevkaSharpieTerm";
+          fixed-ttf = makeIosevkaFont "sharpie-fixed" "ttf" "IosevkaSharpieFixed";
+        };
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
